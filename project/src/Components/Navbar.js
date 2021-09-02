4802b26b901202032f9be506c977b3c50cf37bcf
farazzhh <havaee.faraz@gmail.com>
2021-09-02 19:11:09 +0430

    set state for darkMode and condition to change it

--- project/src/Components/Navbar.js
+++ project/src/Components/Navbar.js
@@ -1,15 +1,34 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-const Navbar = () => {
+import { Link } from "react-router-dom";
+const Navbar = ({ darkMode, darkModeHanlder }) => {
+  const styles = {
+    right: 0,
+  };
   return (
-    <nav className="w-screen h-16 py-2 bg-gray-100">
-      <div className="container mx-auto">
-        <Link to="/">
-          <a>
-            <img src="" alt="logo" className />
-            <span>FRZH</span>
-          </a>
+    <nav className="w-screen py-2 bg-gray-100 dark:bg-gray-900">
+      <div className="container mx-auto flex justify-between">
+        <Link to="/" className="flex items-center">
+          <img src="./assets/logo.png" alt="logo" className="w-16" />
+          <span>FRZH</span>
         </Link>
+        <div className="flex items-center ">
+          <span> day</span>
+          <label htmlFor="darkMode cursor-pointer" onClick={darkModeHanlder}>
+            <div className="relative flex items-center mx-2">
+              <input type="checkbox" className="hidden" id="darkMode" />
+              <div className="w-16 h-8 bg-gray-300 rounded-full"></div>
+
+              <div
+                className={`${
+                  darkMode
+                    ? "transition duration-300 ease-in-out w-8 h-8 bg-blue-600 absolute rounded-full right-0"
+                    : "transition duration-300 ease-in-out w-8 h-8 bg-blue-600 absolute rounded-full translate-x-3"
+                }`}
+              ></div>
+            </div>
+          </label>
+          <span>night</span>
+        </div>
       </div>
     </nav>
   );
--- project/src/Pages/index.js
+++ project/src/Pages/index.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../Components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
-    const [darkMode,setDarkMode] = useState(false)
-    return (
-        <>
-<Navbar />
-        </>
-    )
-}
+  const [darkMode, setDarkMode] = useState(false);
 
-export default Home
+    const darkModeHanlder = () => {
+        setDarkMode(!darkMode)
+        console.log("clicked");
+  }
+  return (
+    <>
+      <Navbar darkMode={darkMode} darkModeHanlder={darkModeHanlder} />
+    </>
+  );
+};
+
+export default Home;
