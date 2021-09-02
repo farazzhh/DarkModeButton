454904357888b6d9903cc9169310c189b9c6ebde
farazzhh <havaee.faraz@gmail.com>
2021-09-02 15:31:42 +0430

    install and run tailwind

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import Home from "./Pages";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
