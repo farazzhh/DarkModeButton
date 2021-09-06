36f2d410bcb391ca8b3368b71713cc46c87537ac
farazzhh <havaee.faraz@gmail.com>
2021-09-07 00:06:06 +0430

    edit app.js

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Home from "./Pages";
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./Pages/About";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const darkModeHanlder = () => {
+    setDarkMode(!darkMode);
+    console.log("clicked");
+  };
+  
   return (
-    <Router>
-      <Home />
+    <Router  className={`${!darkMode && `dark`}`}>
+      <Navbar darkMode={darkMode} darkModeHanlder={darkModeHanlder} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Footer/>
     </Router>
   );
 }
