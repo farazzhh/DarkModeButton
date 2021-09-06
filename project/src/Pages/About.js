c2274cbcc6128b246e6de59a8432062d9b667400
farazzhh <havaee.faraz@gmail.com>
2021-09-07 00:13:14 +0430

    edit about

--- project/src/Pages/About.js
+++ project/src/Pages/About.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Sections from '../Components/Sections'
+import React from "react";
+import Sections from "../Components/Sections";
 const About = () => {
-    return (
-        <div>
-            <Sections />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Sections side={false} src="./assets/section2.jpg" />
+      <Sections side={true} src="./assets/section1.jpg" />
+    </div>
+  );
+};
 
-export default About
-
+export default About;
