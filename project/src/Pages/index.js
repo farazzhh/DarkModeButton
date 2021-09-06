52ff39edff2bb02122eb3678cb01251bca76779f
farazzhh <havaee.faraz@gmail.com>
2021-09-07 00:06:31 +0430

    edit Homepage

--- project/src/Pages/index.js
+++ project/src/Pages/index.js
@@ -5,19 +5,14 @@ import Navbar from "../Components/Navbar";
 import Sections from "../Components/Sections";
 
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-    const darkModeHanlder = () => {
-        setDarkMode(!darkMode)
-        console.log("clicked");
-  }
+  
   return (
-    <div className={`${!darkMode && `dark`}`}>
-      <Navbar darkMode={darkMode} darkModeHanlder={darkModeHanlder} />
+    <div>
+     
       <Header />
       <Sections  side={false} src="./assets/section1.jpg" />
       <Sections  side={true} src="./assets/section2.jpg" />
-      <Footer  />
+     
     </div>
   );
 };
